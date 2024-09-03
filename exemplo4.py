d27c564027f025b74987ce4217368e1401d539cb
dudão <dudag449@gmail.com>
2024-09-03 12:11:01 -0300

    calcular e apresentar o valor do volume de uma caixa retangular

---
+++ exemplo4.py
@@ -0,0 +1,7 @@
+COMPRIMENTO = float(input("informe o comprimento da caixa(em und):"))
+LARGURA = float(input("informe a largura da caixa(em und):"))
+ALTURA = float(input("informe a altura da caixa(em und):"))
+
+VOLUME = (COMPRIMENTO*LARGURA*ALTURA)
+
+print(f"o volume da caixa é{VOLUME}")
